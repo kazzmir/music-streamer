018e1adbae463d55f5f4dd0f3bb5588aabdf82a1
Jon Rafkind <jon@rafkind.com>
2022-07-31 23:34:03 -0700

    decode stream and send it to the sdl audio device

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/kazzmir/music-streamer
 
 go 1.18
 
-require github.com/hajimehoshi/go-mp3 v0.3.3 // indirect
+require (
+	github.com/hajimehoshi/go-mp3 v0.3.3 // indirect
+	github.com/veandco/go-sdl2 v0.4.24 // indirect
+)
--- test/main.go
+++ test/main.go
@@ -4,36 +4,110 @@ import (
     "net/http"
     "log"
     "github.com/hajimehoshi/go-mp3"
+    "github.com/veandco/go-sdl2/sdl"
 )
 
-func main() {
-    log.SetFlags(log.Ldate | log.Lshortfile | log.Lmicroseconds)
-    url := "http://ice4.somafm.com/groovesalad-128-mp3"
+func setupAudio(sampleRate float32, format sdl.AudioFormat) (sdl.AudioDeviceID, error) {
+    var audioSpec sdl.AudioSpec
+    var obtainedSpec sdl.AudioSpec
+
+    audioSpec.Freq = int32(sampleRate)
+    audioSpec.Format = format
+    audioSpec.Channels = 2
+    audioSpec.Samples = 1024
+    // audioSpec.Callback = sdl.AudioCallback(C.generate_audio_c)
+    audioSpec.Callback = nil
+    audioSpec.UserData = nil
+
+    var device sdl.AudioDeviceID
+    var err error
+    sdl.Do(func(){
+        device, err = sdl.OpenAudioDevice("", false, &audioSpec, &obtainedSpec, sdl.AUDIO_ALLOW_FORMAT_CHANGE)
+    })
+    return device, err
+}
+
+func stream(url string) error {
+
+    log.Printf("Init")
+    var err error
+    sdl.Do(func(){
+        err = sdl.Init(sdl.INIT_AUDIO)
+    })
+
+    if err != nil {
+        return err
+    }
+
+    defer sdl.Do(sdl.Quit)
+
+    log.Printf("Opening audio device")
+    var audioDevice sdl.AudioDeviceID
+    audioDevice, err = setupAudio(44100, sdl.AUDIO_S16)
+
+    if err != nil {
+        return err
+    }
+
+    defer sdl.Do(func(){
+        sdl.CloseAudioDevice(audioDevice)
+    })
+
+    log.Printf("Connect to http stream")
+
     response, err := http.Get(url)
     if err != nil {
-        log.Printf("Could not get stream '%v': %v", url, err)
-        return
+        return err
     }
     log.Printf("Connected")
     defer response.Body.Close()
 
     decoder, err := mp3.NewDecoder(response.Body)
     if err != nil {
-        log.Printf("Could not open mp3 decoder: %v", err)
-        return
+        return err
     }
     
     data := make([]byte, 1 << 16)
 
+    sdl.Do(func(){
+        sdl.PauseAudioDevice(audioDevice, false)
+    })
+
     for {
         n, err := decoder.Read(data)
         if err != nil {
-            log.Printf("Could not decode: %v", err)
-            return
+            return err
         } else {
             log.Printf("Decoded %v bytes", n)
         }
+
+        /*
+        useBytes := make([]byte, n)
+        copy(useBytes, data[:n])
+        */
+
+        sdl.Do(func(){
+            // err = sdl.QueueAudio(audioDevice, useBytes)
+            err = sdl.QueueAudio(audioDevice, data[:n])
+        })
+        if err != nil {
+            return err
+        }
     }
+
+    return nil
+}
+
+func main() {
+    log.SetFlags(log.Ldate | log.Lshortfile | log.Lmicroseconds)
+
+    sdl.Main(func(){
+        url := "http://ice4.somafm.com/groovesalad-128-mp3"
+        err := stream(url)
+        if err != nil {
+            log.Printf("Error: %v", err)
+        }
+    })
 
     log.Printf("Done")
 }
